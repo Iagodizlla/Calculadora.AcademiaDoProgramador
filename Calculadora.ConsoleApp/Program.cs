ab327e98cbf334712fa417472ecc187c98a7ef66
Iagodizlla <iagoschlemper@gmail.com>
2025-03-06 20:03:44 -0300

    Novas metas(Raiz, Raiz quadrada e fatorial)

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -2,17 +2,24 @@
 {
     internal class Program
     {
+        /*objetivos;
+         * Descobrir Raiz de numero
+         * Raiz quadrada de um numero
+         * Fatorial de um numero
+         * */
         static void Main(string[] args)
         {
             //Academia do Programador
-            Console.WriteLine("________________________________");
+            Console.WriteLine("__________________________");
             Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("--------------------------------");
-            double n1, n2, res = 0;
+            Console.WriteLine("--------------------------");
+            double n1, n2, res = 1;
+            int n3;
             byte op;
+
             do
             {
-                Console.WriteLine("\nQual opcao vc deseja?\n1. Soma   -2. Subtracao   -3. Multiplicacao   -4. Divisao   -0. Sair");
+                Console.WriteLine("\nQual opcao vc deseja?\n1. Soma   -2. Subtracao   -3. Multiplicacao   -4. Divisao   -5. Fatorial -0. Sair");
                 op = byte.Parse(Console.ReadLine());
                 if (op != 0)
                 {
@@ -38,6 +45,16 @@
                         else
                         {
                             res = n1 / n2;
+                        }
+                        Console.WriteLine("Reultado: " + res);
+                    }else if(op == 5)
+                    {
+                        Console.Write("Digite o numero: ");
+                        n3 = int.Parse(Console.ReadLine());
+
+                        for(int i = 1;i < n3; i++)
+                        {
+                            res *= (double)i;
                         }
                         Console.WriteLine("Reultado: " + res);
                     }
